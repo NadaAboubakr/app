496dc52d97c7d1897addef6d9366b1dd0d6b942d
NadaAboubakr <nadabakr8102@gmail.com>
2022-10-24 11:48:05 +0200

    my commit

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click(View view){
+        TextView text = findViewById(R.id.tt);
+        EditText in = findViewById(R.id.in);
+        text.setText("Hello "+in.getText().toString());
+    }
+}
